517285390bc05da6c34404dfea8662eed4c117c8
lamtailoi2 <lamtailoi11141@gmail.com>
2025-08-25 00:39:40 +0700

    [LoiLT] setup: config axios & create http client

---
+++ src/lib/axios.ts
@@ -0,0 +1,34 @@
+import _axios from "axios";
+
+
+export const axios = _axios.create({
+  // API url
+  baseURL: "https://api.example.com",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Get token from header
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Handle refresh token / logout
+      console.log("Unauthorized, redirect to login...");
+    }
+    return Promise.reject(error);
+  }
+);
---
+++ src/lib/http.ts
@@ -0,0 +1,28 @@
+
+import type { AxiosRequestConfig } from "axios";
+import { axios } from "./axios"
+
+
+export const httpClient = () => {
+    const get = async <T>(url: string, options?: AxiosRequestConfig) => {
+        const res = await axios.get<T>(url, options);
+        return res.data;
+    };
+
+    const post = async <T>(url: string, body?: any, options?: AxiosRequestConfig) => {
+        const res = await axios.post<T>(url, body, options);
+        return res.data;
+    };
+
+    const patch = async <T>(url: string, body?: any, options?: AxiosRequestConfig) => {
+        const res = await axios.patch<T>(url, body, options);
+        return res.data;
+    };
+
+    const del = async <T>(url: string, options?: AxiosRequestConfig) => {
+        const res = await axios.delete<T>(url, options);
+        return res.data;
+    };
+
+    return { get, post, put, patch, del };
+};
