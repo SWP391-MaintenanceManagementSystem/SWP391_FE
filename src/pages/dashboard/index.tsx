db268a07301171d338bf9940032f2dbc5f7b01f8
pnhoanganh <pnhoanganhh@gmail.com>
2025-09-23 18:53:30 +0700

    [AnhPNH] feat: add dashboard role-based rendering

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,38 @@
+import Loading from "@/components/Loading";
+import { useAuth } from "@/contexts/AuthContext";
+import { AccountRole } from "@/types/enums/role";
+import { lazy, Suspense } from "react";
+
+const AdminDashboadComponent = lazy(
+  () => import("./components/AdminDashboard")
+);
+const CustomerDashboadComponent = lazy(
+  () => import("./components/CustomerDashboard")
+);
+
+const roleComponents = {
+  admin: AdminDashboadComponent,
+  user: CustomerDashboadComponent,
+};
 
 export default function Dashboard() {
-    return (
-        <div className="flex ">
-            <h1>DashBoard</h1>
-        </div>
-    )
+  const { auth } = useAuth();
+  const getComponents = () => {
+    switch (auth.user?.role) {
+      case AccountRole.ADMIN:
+        return roleComponents.admin;
+      case AccountRole.CUSTOMER:
+        return roleComponents.user;
+      default:
+        return roleComponents.user;
+    }
+  };
+
+  const Component = getComponents();
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <Component />
+    </Suspense>
+  );
 }
