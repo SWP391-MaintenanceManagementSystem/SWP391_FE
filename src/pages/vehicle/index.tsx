2b50a185f1d891c736dac80f9bfb02905646b06b
pnhoanganh <pnhoanganhh@gmail.com>
2025-09-24 17:12:40 +0700

    [AnhPNH] feat: add AdminVehiclesManagement page with breadcrumbs and layout

--- src/pages/vehicle/index.tsx
+++ src/pages/vehicle/index.tsx
@@ -1,6 +1,32 @@
+import { useAuth } from "@/contexts/AuthContext";
+import { lazy, Suspense } from "react";
+import { AccountRole } from "@/types/enums/role";
+import Loading from "@/components/Loading";
+
+const AdminVehiclesManagement = lazy(
+  () => import("./components/AdminVehiclesManagement"),
+);
+
+const roleComponents = {
+  admin: AdminVehiclesManagement,
+};
 
 export default function Vehicle() {
-    return (
-        <div>Vehicle Page</div>
-    )
+  const { auth } = useAuth();
+
+  const getComponent = () => {
+    switch (auth.user?.role) {
+      case AccountRole.ADMIN:
+        return roleComponents.admin;
+      default:
+        return null;
+    }
+  };
+
+  const Component = getComponent();
+  return (
+    <Suspense fallback={<Loading />}>
+      <Component />
+    </Suspense>
+  );
 }
