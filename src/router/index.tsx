94cfd965dbd0b2638de3f045c6b5228226e60cc7
lamtailoi2 <lamtailoi11141@gmail.com>
2025-10-05 14:21:47 +0700

    [LoiLT] hotfix: block DISABLED and BANNED account login via google

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -17,6 +17,8 @@ import ProfilePage from "@/pages/profile";
 import Unauthorized from "@/pages/unauthorized";
 import ViewDetailInfo from "@/pages/vehicle/components/admin/ViewDetailInfo";
 import MembershipPage from "@/pages/membership";
+import Booking from "@/pages/booking/customer/Booking";
+import AuthFailed from "@/pages/auth/components/AuthFailed";
 
 const RouterComponent = () => {
   const router = createBrowserRouter([
@@ -37,6 +39,7 @@ const RouterComponent = () => {
     },
     { path: "/forgot-password", element: <ForgotPasswordPage /> },
     { path: "/auth/success", element: <AuthSuccess /> },
+    { path: "auth/failed", element: <AuthFailed /> },
     { path: "/auth/verify", element: <VerifySuccess /> },
     { path: "/unauthorized", element: <Unauthorized /> },
     { path: "*", element: <NotFound /> },
