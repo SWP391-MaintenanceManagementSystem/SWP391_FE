3cd8034db8b0929f69886af63cfc82afcae5621e
pnhoanganh <pnhoanganhh@gmail.com>
2025-10-09 09:27:33 +0700

    [AnhPNH] feat: create UI StatisticsSection for Inventory page

---
+++ src/pages/inventory/components/StatisticsCard.tsx
@@ -0,0 +1,29 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import type { LucideIcon } from "lucide-react";
+
+interface Props {
+  title: string;
+  icon: LucideIcon;
+  numberTotal: number;
+  description: string;
+}
+
+export default function StatisticsCard({
+  title,
+  icon: Icon,
+  numberTotal,
+  description,
+}: Props) {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        <Icon className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">{numberTotal}</div>
+        <p className="text-xs text-muted-foreground">{description}</p>
+      </CardContent>
+    </Card>
+  );
+}
--- src/pages/inventory/index.tsx
+++ src/pages/inventory/index.tsx
@@ -1,12 +1,13 @@
 import DynamicBreadcrumbs from "@/components/DynamicBreadcrumb";
 import MainContentLayout from "@/components/MainContentLayout";
+import StatisticsSection from "./components/StatisticsSection";
 
 export default function InventoryManagement() {
   return (
     <div>
-      <DynamicBreadcrumbs />
-      <MainContentLayout>
-        <h3>Inventory Management</h3>
+      <DynamicBreadcrumbs pathTitles={{ inventory: "Inventory Management" }} />
+      <MainContentLayout className="grid grid-cols-1 md:grid-rows-[auto, 1fr]">
+        <StatisticsSection />
       </MainContentLayout>
     </div>
   );
