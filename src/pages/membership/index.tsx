8c13f3d1e899cf12e89cc2a639ed5b62d29418dc
kimhaont <kimhao25052005@gmail.com>
2025-10-12 16:40:47 +0700

    [HaoNTK] feat: update dynamic import by role for Memberships Page

--- src/pages/membership/index.tsx
+++ src/pages/membership/index.tsx
@@ -1,9 +1,29 @@
-import Membership from "./components/customer/Membership";
+import { lazy, Suspense } from "react";
+import { useAuth } from "@/contexts/AuthContext";
+import { AccountRole } from "@/types/enums/role";
+import Loading from "@/components/Loading";
+
+const CustomerMembership = lazy(
+  () => import("./components/customer/Membership")
+);
+// const AdminMembership
 
 export default function MembershipPage() {
-  return (
-    <div>
-        <Membership/>
-    </div>
-  )
+  const { auth } = useAuth();
+
+  // Hàm trả về component phù hợp theo role
+  const getComponentByRole = () => {
+    switch (auth.user?.role) {
+      case AccountRole.CUSTOMER:
+        return <CustomerMembership />;
+
+      case AccountRole.ADMIN:
+        // return <AdminMembership />;
+        return <div>Admin Membership Page (to be implemented)</div>;
+      default:
+        return <div>Unauthorized</div>;
+    }
+  };
+
+  return <Suspense fallback={<Loading />}>{getComponentByRole()}</Suspense>;
 }
