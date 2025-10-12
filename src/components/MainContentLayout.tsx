1c598053c23f40e0ef1a24ca9f68e512e5bb990a
kimhaont <kimhao25052005@gmail.com>
2025-10-12 15:55:22 +0700

    [HaoNTK] feat: add dialog

--- src/components/MainContentLayout.tsx
+++ src/components/MainContentLayout.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function MainContentLayout({ children, className }: Props) {
   return (
     <div
-      className={cn("mt-4 pt-4 pb-12 px-[12px] h-full min-w-screen", className)}
+      className={cn("mt-4 pt-4 pb-12 px-[12px] h-full w-full", className)}
     >
       {children}
     </div>
