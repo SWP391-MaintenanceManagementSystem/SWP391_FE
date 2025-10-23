10e940ca25c49a25641beaf8d3721e4f1eb8edb0
kimhaont <kimhao25052005@gmail.com>
2025-10-23 16:45:21 +0700

    [HaoNTK] feat: change view detail display from dialog to standalone page

--- src/pages/booking/components/technician/booking-detail/AssignedBookingDetail.tsx
+++ src/pages/booking/components/technician/booking-detail/AssignedBookingDetail.tsx
@@ -1,7 +1,142 @@
-import React from "react";
 import { useParams } from "react-router-dom";
+import dayjs from "dayjs";
+import { useState } from "react";
+
+import DynamicBreadcrumbs from "@/components/DynamicBreadcrumb";
+import MainContentLayout from "@/components/MainContentLayout";
+import { useBookingDetail } from "@/services/booking/hooks/useBookingDetail";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Spinner } from "@/components/ui/shadcn-io/spinner";
+import { Button } from "@/components/ui/button";
+import CustomerInfoCard from "./CustomerInfoCard";
+import VehicleInfoCard from "./VehicleInfoCard";
+import ShiftInfoCard from "./ShiftInfoCard";
+import AssignerInfoCard from "./AssignerInfoCard";
 
 export default function AssignedBookingDetail() {
   const { id } = useParams<{ id: string }>();
-  return <div>AssignedBookingDetail {id}</div>;
+  const { data, isLoading } = useBookingDetail(id ?? "");
+
+
+  if (!id) {
+    return <div className="text-red-500 p-6">Booking ID is missing</div>;
+  }
+
+  if (isLoading) {
+    return (
+      <div className="text-gray-500 p-6 flex justify-center items-center h-full">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full min-h-[calc(100vh-32px)] font-inter bg-gradient-to-br">
+      <DynamicBreadcrumbs
+        pathTitles={{
+          booking: "Booking Services",
+          [id]: "Booking Details",
+        }}
+      />
+
+      <MainContentLayout className="p-6">
+        <Card className="mb-6 bg-purple-50 border-purple-200 dark:bg-purple-800 dark:border-purple-800 shadow-md">
+          <CardHeader className="flex flex-row items-center justify-between p-4">
+            <div>
+              <CardTitle className="text-3xl font-bold text-gray-900 dark:text-white flex items-center gap-2">
+                Booking Details
+              </CardTitle>
+              <p>
+                <span className="font-mono font-semibold text-gray-800 dark:text-gray-300 text-xs">
+                  ID: {data?.id || "N/A"}
+                </span>
+              </p>
+            </div>
+
+            {/* Action buttons */}
+            <div className="flex gap-2">
+              <Button
+                onClick={() => setIsModalOpen(true)}
+                variant="outline"
+                className="border-purple-500 text-purple-600 hover:bg-purple-50
+                  dark:border-purple-300 dark:text-purple-200 dark:hover:bg-purple-700/30"
+              >
+                View Task
+              </Button>
+            </div>
+          </CardHeader>
+
+          <CardContent className="p-4 space-y-4 md:space-y-0">
+            <div className="flex flex-col md:flex-row justify-between text-gray-700 dark:text-gray-50 space-y-4 md:space-y-0">
+              <p>
+                <strong>Status:</strong>{" "}
+              </p>
+              <p>
+                <strong>Center:</strong> {data?.serviceCenter?.name || "N/A"} -{" "}
+                {data?.serviceCenter?.address || "N/A"}
+              </p>
+            </div>
+
+            <div className="flex flex-col md:flex-row justify-between text-gray-700 dark:text-gray-50 space-y-4 md:space-y-0">
+              <p>
+                <strong>Total Cost:</strong>{" "}
+                {data?.totalCost
+                  ? `$${Number(data.totalCost).toLocaleString("en-US")}`
+                  : "No cost information available."}
+              </p>
+              <p>
+                <strong>Date:</strong>{" "}
+                {data?.bookingDate
+                  ? dayjs(data.bookingDate).format("DD/MM/YYYY HH:mm")
+                  : "N/A"}
+              </p>
+            </div>
+
+            {data?.note && (
+              <div className="pt-3 text-gray-700 dark:text-gray-200">
+                <strong>Note:</strong> {data.note}
+              </div>
+            )}
+          </CardContent>
+        </Card>
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <CustomerInfoCard
+            firstName={data?.customer?.firstName}
+            lastName={data?.customer?.lastName}
+            email={data?.customer?.email}
+            phone={data?.customer?.phone}
+          />
+
+          <VehicleInfoCard
+            id={data?.vehicle?.id}
+            licensePlate={data?.vehicle?.licensePlate}
+            vin={data?.vehicle?.vin}
+            model={data?.vehicle?.model}
+            brand={data?.vehicle?.brand}
+            productionYear={data?.vehicle?.productionYear}
+          />
+
+          <ShiftInfoCard
+            shift={{
+              id: data?.shift?.id ?? "",
+              name: data?.shift?.name ?? "N/A",
+              startTime: data?.shift?.startTime
+                ? new Date(data.shift.startTime)
+                : new Date(),
+              endTime: data?.shift?.endTime
+                ? new Date(data.shift.endTime)
+                : new Date(),
+            }}
+          />
+          <AssignerInfoCard
+            firstName={data?.staff?.firstName}
+            lastName={data?.staff?.lastName}
+            email={data?.staff?.email}
+          />
+        </div>
+      </MainContentLayout>
+    </div>
+  );
 }
---
+++ src/pages/booking/components/technician/booking-detail/VehicleInfoCard.tsx
@@ -0,0 +1,72 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CarFront } from "lucide-react"; // Thêm icon từ lucide-react
+
+interface VehicleInfoCardProps {
+  id?: string;
+  licensePlate?: string;
+  vin?: string;
+  model?: string;
+  brand?: string;
+  productionYear?: number;
+}
+
+export default function VehicleInfoCard({
+  licensePlate,
+  vin,
+  model,
+  brand,
+  productionYear,
+}: VehicleInfoCardProps) {
+  return (
+    <Card className="w-full hover:shadow-lg dark:hover:shadow-gray-700 transition-shadow duration-300 border border-gray-200 dark:border-[#262626] rounded-lg overflow-hidden">
+      <CardHeader className="flex flex-row items-center gap-2 p-4 bg-gradient-to-r from-purple-50 to-gray-50 dark:from-purple-800 dark:to-purple-500">
+        <CarFront className="w-6 h-6 text-purple-500 dark:text-purple-100" />
+        <CardTitle className="text-lg font-semibold text-gray-900 dark:text-white">
+          {licensePlate || "N/A"}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="p-4 flex flex-col gap-4">
+        <CardDescription className="flex flex-col gap-2 text-gray-700 dark:text-gray-200">
+          <div className="flex items-center gap-2">
+            <span className="font-medium text-gray-600 dark:text-gray-200 w-24">
+              Model:
+            </span>
+            <span className="text-gray-900 dark:text-gray-200">
+              {model || "N/A"}
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="font-medium text-gray-600 w-24 dark:text-gray-200">
+              Brand:
+            </span>
+            <span className="text-gray-900 dark:text-gray-200">
+              {brand || "N/A"}
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="font-medium text-gray-600 w-24 dark:text-gray-200">
+              VIN:
+            </span>
+            <span className="text-gray-900 dark:text-gray-200">
+              {vin || "N/A"}
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="font-medium text-gray-600 w-24 dark:text-gray-200">
+              Year:
+            </span>
+            <span className="text-gray-900 dark:text-gray-200">
+              {productionYear || "N/A"}
+            </span>
+          </div>
+        </CardDescription>
+      </CardContent>
+    </Card>
+  );
+}
