34fe7014005eb208bc9007a0b38e2d19bdfdcc7c
pnhoanganh <pnhoanganhh@gmail.com>
2025-10-27 19:20:46 +0700

    [AnhPNH] feat: add productionYear in VehicleInfoBox

--- src/pages/vehicle/components/manager/vehicleManagement/VehicleInfoBox.tsx
+++ src/pages/vehicle/components/manager/vehicleManagement/VehicleInfoBox.tsx
@@ -5,67 +5,87 @@ import vehicleImg from "@/assets/vehicle_img.png";
 import dayjs from "dayjs";
 import { useGetCustomerById } from "@/services/manager/queries";
 import { InfoSection } from "@/pages/profile/components/InfoSection";
+import "animate.css";
+import { Loader } from "lucide-react";
+
 type VehicleInfoBoxProps = {
   vehicleId: string;
 };
 
 const VehicleInfoBox = ({ vehicleId }: VehicleInfoBoxProps) => {
-  const { data: vehicle } = useGetVehicleById(vehicleId);
-  const { data: customer } = useGetCustomerById(vehicle?.customerId || "");
+  const { data: vehicle, isLoading: isVehicleLoading } =
+    useGetVehicleById(vehicleId);
+  const { data: customer, isLoading: isCustomerLoading } = useGetCustomerById(
+    vehicle?.customerId || "",
+  );
 
   return (
-    <Card className="w-full max-w-sm md:max-w-xs lg:max-w-[368px] h-full">
-      <CardHeader>
-        <CardTitle className="dark:text-white relative flex items-center text-xl">
-          {vehicle?.licensePlate}
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-2">
-        <img
-          src={vehicleImg}
-          alt="Vehicle Image"
-          className="w-full h-40 object-cover rounded "
-        />
-        <InfoSection title="Information">
-          <p>
-            <strong>Owner: </strong>
-            {customer?.profile
-              ? `${customer.profile.firstName} ${customer.profile.lastName}`
-              : ""}
-          </p>
+    <Card className="w-full max-w-sm md:max-w-xs lg:max-w-[368px] h-full min-w-[300px]">
+      {isCustomerLoading || isVehicleLoading ? (
+        <div className="flex justify-center items-center h-full">
+          <Loader className="animate-spin text-gray-500 mx-auto" />
+        </div>
+      ) : (
+        <>
+          <CardHeader>
+            <CardTitle className="dark:text-white relative flex items-center text-xl">
+              {vehicle?.licensePlate}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2">
+            <img
+              src={vehicleImg}
+              alt="Vehicle Image"
+              className="w-full h-40 object-cover rounded "
+            />
+            <InfoSection title="Information">
+              <p>
+                <strong>Owner: </strong>
+                {customer?.profile
+                  ? `${customer.profile.firstName} ${customer.profile.lastName}`
+                  : ""}
+              </p>
 
-          <p>
-            <strong>Email: </strong>
-            {customer?.email || ""}
-          </p>
-          <p>
-            <strong>Brand: </strong>
-            {vehicle?.brand || ""}
-          </p>
-          <p>
-            <strong>Model: </strong>
-            {vehicle?.model || ""}
-          </p>
-          <p>
-            <strong>Vin: </strong>
-            {vehicle?.vin || ""}
-          </p>
-          <p className="font-inter flex flex-row gap-2">
-            <strong className="font-inter">Status:</strong>
-            <span className="inline-flex">
-              {vehicle?.status && <VehicleStatusTag status={vehicle.status} />}
-            </span>
-          </p>
-          <p>
-            <strong>Created: </strong>
-            {dayjs(vehicle?.createdAt).format("HH:mm:ss DD/MM/YYYY") || ""}
-          </p>
-          <p>
-            <strong>Updated: </strong>
-            {dayjs(vehicle?.updatedAt).format("HH:mm:ss DD/MM/YYYY") || ""}
-          </p>
-        </InfoSection>
-      </CardContent>
+              <p>
+                <strong>Email: </strong>
+                {customer?.email || ""}
+              </p>
+              <p>
+                <strong>Brand: </strong>
+                {vehicle?.brand || ""}
+              </p>
+              <p>
+                <strong>Model: </strong>
+                {vehicle?.model || ""}
+              </p>
+              <p>
+                <strong>Vin: </strong>
+                {vehicle?.vin || ""}
+              </p>
+              <p>
+                <strong>Production Year: </strong>
+                {vehicle?.productionYear || ""}
+              </p>
+              <p className="font-inter flex flex-row gap-2">
+                <strong className="font-inter">Status:</strong>
+                <span className="inline-flex">
+                  {vehicle?.status && (
+                    <VehicleStatusTag status={vehicle.status} />
+                  )}
+                </span>
+              </p>
+              <p>
+                <strong>Created: </strong>
+                {dayjs(vehicle?.createdAt).format("HH:mm:ss DD/MM/YYYY") || ""}
+              </p>
+              <p>
+                <strong>Updated: </strong>
+                {dayjs(vehicle?.updatedAt).format("HH:mm:ss DD/MM/YYYY") || ""}
+              </p>
+            </InfoSection>
+          </CardContent>
+        </>
+      )}
     </Card>
   );
 };
