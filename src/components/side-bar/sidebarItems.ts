e85272e7c14b8457ec3c5bad2cf86a9d36cb1606
pnhoanganh <pnhoanganhh@gmail.com>
2025-11-06 20:27:21 +0700

    [AnhPNH] feat: show notifications for customer, staff, technician

--- src/components/side-bar/sidebarItems.ts
+++ src/components/side-bar/sidebarItems.ts
@@ -50,12 +50,9 @@ export const customerItems: SidebarItem[] = [
   { title: "My Vehicles", url: "/vehicles", icon: Car },
   { title: "Memberships", url: "/membership", icon: UserStar },
   { title: "Service Booking", url: "/booking", icon: NotebookPen },
-  { title: "Maintenance History", url: "/history", icon: BookOpen },
-  { title: "Payments", url: "/payments", icon: CreditCard },
-  { title: "Settings", url: "/settings", icon: Settings },
-  { title: "Help & Support", url: "/support", icon: CircleQuestionMark },
   { title: "Chat Box", url: "/chat", icon: MessageCircle },
-  { title: "Feedback", url: "/feedback", icon: Sparkles },
+  { title: "Notifications", url: "/notification", icon: Bell },
+  { title: "Help & Support", url: "/support", icon: CircleQuestionMark },
 ];
 
 export const staffItems: SidebarItem[] = [
@@ -64,10 +61,12 @@ export const staffItems: SidebarItem[] = [
   { title: "Booking Management", url: "/booking", icon: BookOpenCheckIcon },
   { title: "Customers & Vehicles", url: "/vehicles", icon: Users },
   { title: "Chat Box", url: "/chat", icon: MessageCircle },
+  { title: "Notifications", url: "/notification", icon: Bell },
 ];
 export const technicianItems: SidebarItem[] = [
   { title: "Dashboard", url: "/dashboard", icon: Home },
   { title: "Work Schedule", url: "/viewSchedule", icon: CalendarRange },
   { title: "My Assigned Bookings", url: "/booking", icon: CalendarClock },
   { title: "Inventory", url: "/inventory", icon: PackageOpen },
+  { title: "Notifications", url: "/notification", icon: Bell },
 ];
