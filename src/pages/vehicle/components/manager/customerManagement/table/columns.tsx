b78bff8f82ead7c1ce9008f9f2069f87733584de
pnhoanganh <pnhoanganhh@gmail.com>
2025-11-20 20:41:15 +0700

    [AnhPNH] refactor: replace checkbox column with No column in data table

--- src/pages/vehicle/components/manager/customerManagement/table/columns.tsx
+++ src/pages/vehicle/components/manager/customerManagement/table/columns.tsx
@@ -1,5 +1,4 @@
 import { createColumnHelper } from "@tanstack/react-table";
-import { Checkbox } from "@/components/ui/checkbox";
 import SortHeader from "@/components/table/SortHeader";
 import FilterHeader from "@/components/table/FilterHeader";
 import AccountStatusTag from "@/components/tag/AccountStatusTag";
@@ -17,28 +16,15 @@ export const getColumns = (
 ) => {
   const columnHelper = createColumnHelper<CustomerTable>();
   return [
-    // SELECT checkbox
     columnHelper.display({
-      id: "select",
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && "indeterminate")
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-          className="!outline-none"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-          className="!outline-none"
-        />
-      ),
+      id: "number",
+      header: "#",
+      size: 50,
+      cell: ({ row, table }) =>
+        row.index +
+        1 +
+        table.getState().pagination.pageIndex *
+          table.getState().pagination.pageSize,
       enableSorting: false,
       enableHiding: false,
     }),
